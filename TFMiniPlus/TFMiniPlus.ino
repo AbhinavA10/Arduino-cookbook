b6b7ceeefd205dcf0b7282f048166403ff9f62f9
Abhinav Agrahari <abhiraja1012@gmail.com>
2022-01-06 18:34:40 -0500

    Update comments in TFMini example

--- TFMiniPlus/TFMiniPlus.ino
+++ TFMiniPlus/TFMiniPlus.ino
@@ -1,22 +1,23 @@
-//#include<SoftwareSerial.h>
-//SoftwareSerial Serial3(10, 9); //Set Transmit and Recieve pins, pin 2 is RX, pin 3 is TX
+// Demonstrates reading from TFMiniPlus sensor over UART
+
+int uart[9];
 
 int dist;
 int strength;
-int check;
-int uart[9];
-int i;
-const int HEADER = 0x59;
+int temp;
 
+int check;
+int i;
+
+const int HEADER = 0x59; // Frame header in data
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   Serial3.begin(115200);
-  Serial.println("start");
 }
 
 void loop() {
-  if (Serial3.available()>=9) {
+  // Data structure to read in is based on datasheet
+  if (Serial3.available() >= 9) {
     if (Serial3.read() == HEADER) {
       uart[0] = HEADER;
       if (Serial3.read() == HEADER) {
@@ -25,9 +26,10 @@ void loop() {
           uart[i] = Serial3.read();
         }
         check = uart[0] + uart[1] + uart[2] + uart[3] + uart[4] + uart[5] + uart[6] + uart[7];
-        if (uart[8] == (check & 0xff)) {
-          dist = uart[2] + uart[3] * 256;
+        if (uart[8] == (check & 0xff)) { // checksum (byte 8) is lower 8 bits of sum of first 8 bytes
+          dist = uart[2] + uart[3] * 256; // in cm
           strength = uart[4] + uart[5] * 256;
+          temp = uart[6] + uart[7] * 256;
           Serial.println(dist);
           delay(10);
         }
