a8bb1c69a39e413e25b2ec25374db52b9bfd1984
Ashish <shinyflame007@gmail.com>
2023-02-21 00:44:20 -0500

    Added DHT11 code and libraries

---
+++ DHT11_AirTemp/DHT11_Air_and_Humidity/DHT11_Air_and_Humidity.ino
@@ -0,0 +1,40 @@
+/* How to use DHT-11 sensor with Arduino uno
+   Temperature and humidity sensor
+   Dev: Michalis Vasilakis // Date: 1/7/2015 // www.ardumotive.com */
+
+//Libraries
+#include <DHT.h>
+
+//Constants
+#define DHTPIN 2     // what pin we're connected to
+#define DHTTYPE DHT11   // DHT 11  (AM2302)
+// Initialize DHT sensor for normal 16mhz Arduino
+DHT dht(DHTPIN, DHTTYPE);
+
+//Variables
+int chk;
+float hum;  //Stores humidity value
+float temp; //Stores temperature value
+
+void setup()
+{
+    Serial.begin(9600);
+  dht.begin();
+    
+}
+
+void loop()
+{
+    //Read data and store it to variables hum and temp
+    hum = dht.readHumidity();
+    temp= dht.readTemperature();
+    //Print temp and humidity values to serial monitor
+    Serial.print("Humidity: ");
+    Serial.print(hum);
+    Serial.print(" %, Temp: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+    delay(2000); //Delay 2 sec.
+}
+
+   
---
+++ DHT11_AirTemp/readme.md
@@ -0,0 +1,5 @@
+# DHT11 Air Temperature and Humidity
+
+- Download the `Adafruit_DHT` library (it will automatically ask you to also download the `Adafruit_Unified_Sensor` libary)
+- OR: Unzip the 2 libraries, and place in `\Documents\Arduino\libraries`
+- For the Raspberry Pi, checkout the `DHT11_Python-master.zip` or go to https://github.com/szazo/DHT11_Python
